f58de7fde99588d9f3e520c924de627a87e7e0ba
Sagnik Chandra <sagnikc2003@gmail.com>
2026-07-01 17:59:56 +0530

    generator code in progress

--- TREC-RAG_2024_Analysis/Discriminator_and_Noise/Discriminator/Generator/generator.py
+++ TREC-RAG_2024_Analysis/Discriminator_and_Noise/Discriminator/Generator/generator.py
@@ -3,6 +3,7 @@ from gold_injector import gold_injector
 from prompt_creator import prompt_creator
 import json
 import gzip
+import torch
 
 #####################
 with open(r'/home/irlab/sagnik/API_KEY','r') as f:
@@ -12,7 +13,7 @@ with open(r'/home/irlab/sagnik/API_KEY','r') as f:
 model_name="mistralai/Mistral-7B-Instruct-v0.3"
 
 
-model=AutoModelForCausalLM.from_pretrained(model_name,token=hf_token,torch_dtype=torch.bfloat16,attn_implementation="flash_attention_2")
+model=AutoModelForCausalLM.from_pretrained(model_name,token=hf_token,torch_dtype=torch.bfloat16,attn_implementation="sdpa")
 tokenizer=AutoTokenizer.from_pretrained(model_name,token=hf_token)
 
 # retr_set=[]
@@ -42,7 +43,7 @@ with open(r'/home/irlab/sagnik/TREC-RAG_2024_Analysis/Discriminator_and_Noise/Di
 inputs=tokenizer(prompt,return_tensors="pt")
 input_size=inputs['input_ids'].shape[1]
 
-output_ids=model.generate(inputs['input_ids'],tokenizer=tokenizer, do_sample=False, temperature=0.0)
+output_ids=model.generate(inputs['input_ids'],tokenizer=tokenizer, do_sample=False, temperature=0.0, min_length=400, min_new_tokens=100)
 output1=output_ids[0][input_size:]
 
 output=tokenizer.decode(output1,skip_special_tokens=True)
