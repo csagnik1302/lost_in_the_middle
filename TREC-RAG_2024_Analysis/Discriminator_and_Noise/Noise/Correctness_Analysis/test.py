336d206288286f6f7ed122ae8674cc81dce46c50
Sagnik Chandra <sagnikc2003@gmail.com>
2026-07-15 14:43:49 +0530

    missing points put in the pipeline again

--- TREC-RAG_2024_Analysis/Discriminator_and_Noise/Noise/Correctness_Analysis/test.py
+++ TREC-RAG_2024_Analysis/Discriminator_and_Noise/Noise/Correctness_Analysis/test.py
@@ -18,3 +18,32 @@
 #     l={"hi":"Hello"My name""}
 # except Exception as e:
 #     print('Error')
+
+import json
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import json
+import gzip
+import torch
+import re
+import os
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings(
+    "ignore",
+    category=FutureWarning,
+    module="bitsandbytes"
+)
+import sys
+
+input=[]
+
+with open(r'/home/irlab/sagnik/TREC-RAG_2024_Analysis/Discriminator_and_Noise/Noise/Correctness_Analysis/misc/pipeline_error_log_mistral-7b-instruct-v0.3-bnb-4bit.jsonl','r') as f:
+    for i in f:
+        input.append(json.loads(i))
+
+indices_missed=[]
+
+for i in input:
+    indices_missed.append([i['first_gold_doc_pos'],i['corpus_position']])
+
+print(indices_missed)
